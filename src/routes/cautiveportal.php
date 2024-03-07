7156ff42b6b191fbf49d4e808105428cb0b3b332
Juan Daniel Gutierrez Botero <danielcore5@gmail.com>
2024-03-07 17:20:38 -0500

    * refactor: change method to get

--- src/routes/cautiveportal.php
+++ src/routes/cautiveportal.php
@@ -7,7 +7,7 @@ Route::group(['as' => 'cautiveportal.', 'middleware' => ['web']], function () {
     Route::post('/store', ['uses' => $namespacePrefix . 'CautivePortalController@store', 'as' => 'store']);
     Route::match(['GET', 'POST'], '/afterstore', ['uses' => $namespacePrefix . 'CautivePortalController@afterStore', 'as' => 'afterstore']);
     Route::get('/success', ['uses' => $namespacePrefix . 'CautivePortalController@success', 'as' => 'success']);
-    Route::match(['GET', 'POST'], '/validate-otp', ['uses' => $namespacePrefix . 'CautivePortalController@validateOtp', 'as' => 'validateotp']);
+    Route::get('/validate-otp', ['uses' => $namespacePrefix . 'CautivePortalController@validateOtp', 'as' => 'validateotp']);
 
     //Asset Routes
     Route::get('cautiveportal-assets', ['uses' => '\Netmask\CautivePortal\Controllers\\CautivePortalAssetsController@assets', 'as' => 'cautiveportal_assets']);
